2887d260eb0caa2126ec9ad3367677d35b35775f
Puteri Yudani <puteriyudani@gmail.com>
2022-03-18 09:39:39 +0700

    Percobaan Array 2 Dimensi

---
+++ src/main/kotlin/4_Array2Dimensi.kt
@@ -0,0 +1,35 @@
+fun main() {
+    //inisialisasi array 2D
+    var cinema = arrayOf<Array<Int>>()
+    for (i in 0..4) {
+        var array = arrayOf<Int>()
+        for (j in 0..4) {
+            array += 0
+        }
+        cinema += array
+    }
+
+    for (array in cinema) {
+        for (value in array) {
+            print("$value ")
+        }
+        println()
+    }
+
+    //isi dengan data
+    cinema[2][2] = 1
+    for (i in 1..3) {
+        cinema[3][i] = 1
+    }
+    for (i in 0..4) {
+        cinema[4][i] = 1
+    }
+
+    println()
+    for (array in cinema) {
+        for (value in array) {
+            print("$value ")
+        }
+        println()
+    }
+}
