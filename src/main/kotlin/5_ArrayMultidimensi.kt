d99610aa7837d7518718330eab3a35ca2c9e43fa
Puteri Yudani <puteriyudani@gmail.com>
2022-03-18 09:47:08 +0700

    Percobaan Array Multidimensi

---
+++ src/main/kotlin/5_ArrayMultidimensi.kt
@@ -0,0 +1,30 @@
+fun main() {
+    // array multidimensi
+    var cinemas = arrayOf<Array<Array<Int>>>()
+    for (i in 0..2) {
+        var cinema = arrayOf<Array<Int>>()
+        for (j in 0..4) {
+            var array = arrayOf<Int>()
+            for (k in 0..4) {
+                array += 0
+            }
+            cinema += array
+        }
+        cinemas += cinema
+    }
+
+    cinemas[1][2][3] = 1 //cinema lantai 2, baris ke 3, kursi ke 4
+
+    var floor = 1
+    for (cinema in cinemas) {
+        println("Floor: $floor")
+        floor += 1
+        for (array in cinema) {
+            for (value in array) {
+                print("$value ")
+            }
+            println()
+        }
+        println("-----------------")
+    }
+}
