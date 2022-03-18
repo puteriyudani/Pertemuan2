7f293e5c41924314a60691897445aa0b3e6a69cd
Puteri Yudani <puteriyudani@gmail.com>
2022-03-18 11:12:24 +0700

    Percobaan listOf

---
+++ src/main/kotlin/7_ListOf.kt
@@ -0,0 +1,20 @@
+fun main() {
+    //inisialisasi listOf
+    val numbers = listOf("one", "two", "three", "four")
+    //perintah untuk tampilkan program
+    println("Number of elements: ${numbers.size}")
+    println("Third element: ${numbers.get(2)}")
+    println("Fourth element: ${numbers[3]}")
+    println("Index of element \"two\" ${numbers.indexOf("two")}")
+
+    //daftar element (termasuk nol) dapat di duplikasi
+    val bob = Person("Bob", 31)
+    val people = listOf<Person>(Person("Adam", 20), bob, bob)
+    val people2 = listOf<Person>(Person("Adam", 20), Person("Bob", 31), bob)
+    println(people == people2)
+    bob.age = 32
+    println(people == people2)
+}
+
+//membuat class Person
+class Person(var name: String, var age: Int){}
